9e1e10c6417e3436a16a42ca6af544e491824c00
unknown <tve20cs037@cet.ac.in>
2024-10-11 01:14:25 +0530

    changed code spaces

--- function.py
+++ function.py
@@ -1,23 +1,23 @@
 import pandas as pd
 from data import owners, influencers 
 def compare_categories(owner, user):
-    owner_categories = set(c["category_name"] for c in owner["preference"].get("categories", []))
-    user_categories = set(c["category_name"] for c in user["preference"].get("categories", []))
+    owner_categories=set(c["category_name"] for c in owner["preference"].get("categories",[]))
+    user_categories=set(c["category_name"] for c in user["preference"].get("categories",[]))
     return {
         "intersec":len(owner_categories & user_categories),
         "total":len(owner_categories)+len(user_categories)
     }
     
 def compare_products(owner, user):
-    owner_products = set(c["product_name"] for c in owner["preference"].get("products", []))
-    user_products = set(c["product_name"] for c in user["preference"].get("products", []))
+    owner_products=set(c["product_name"] for c in owner["preference"].get("products",[]))
+    user_products=set(c["product_name"] for c in user["preference"].get("products",[]))
     return {
         "intersec":len(owner_products & user_products),
         "total":len(owner_products)+len(user_products)
     }
 def compare_marketing(owner, user):
-    owner_marketing = set(c["marketing_type"] for c in owner["preference"].get("marketing", []))
-    user_marketing = set(c["marketing_type"] for c in user["preference"].get("marketing", []))
+    owner_marketing=set(c["marketing_type"] for c in owner["preference"].get("marketing",[]))
+    user_marketing=set(c["marketing_type"] for c in user["preference"].get("marketing",[]))
     return {
         "intersec":len(owner_marketing & user_marketing),
         "total":len(owner_marketing)+len(user_marketing)
@@ -37,14 +37,14 @@ def content_based_filtering():
         marketing_score=compare_marketing(owner,user)
         
         
-        if product_score["total"] !=0:
+        if product_score["total"]!=0:
             scores.append(round(2*product_score["intersec"]/product_score["total"],2))
-        if category_score["total"] !=0:
+        if category_score["total"]!=0:
             scores.append(round(2*category_score["intersec"]/category_score["total"],2))
-        if marketing_score["total"] !=0:
+        if marketing_score["total"]!=0:
             scores.append(round(2*marketing_score["intersec"]/marketing_score["total"],2))
         
-        result=round(sum(scores) / len(scores), 2) if scores else 0
+        result=round(sum(scores)/len(scores), 2) if scores else 0
         
         arr.append({
             "id":user["id"],
@@ -57,5 +57,6 @@ def content_based_filtering():
     
     for influencer_id in preference_array["id"]:
         print(influencers_df[influencers_df["id"]==influencer_id])
+        
 
 content_based_filtering()
